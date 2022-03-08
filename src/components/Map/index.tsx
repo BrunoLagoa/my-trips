63a4da5f1ca9afc4566398ce8e0a1449d0742ef0
Bruno Castro <miroldols@gmail.com>
2022-03-08 18:44:38 -0300

    Limitano zoom no map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -45,6 +45,12 @@ const Map = ({ places }: MapProps) => {
         zoom={3}
         style={{ height: '100%', width: '100%' }}
         attributionControl={false}
+        minZoom={3}
+        // worldCopyJump={true}
+        maxBounds={[
+          [-180, 180],
+          [180, -180]
+        ]}
       >
         <CustomTileLayer />
 
